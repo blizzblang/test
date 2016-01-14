686a5828b2bb6be455a4ccfb8c4d254ca1e99c99
NWhite269 <NWhite269@FH-C107-SD01.fhsd.local>
2016-01-14 10:25:59 -0600

    Added slang

--- src/com/nathan/main/Main.java
+++ src/com/nathan/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] a)
 	{
 		System.out.println("Hello World, my favorite color is green");
+		System.out.println("Do I look like I know what a jpeg is?");
 	}
 }
