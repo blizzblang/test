6f68f1c8fb03a6c6b7f8077549cf0c2d0b126d9a
NWhite269 <NWhite269@FH-C107-SD01.fhsd.local>
2016-01-14 10:36:18 -0600

    Added Byte Class

---
+++ src/com/nathan/main/UnsignedByte.java
@@ -0,0 +1,38 @@
+package com.nathan.main;
+
+public class UnsignedByte 
+{
+	private short Data;
+	public UnsignedByte()
+	{
+		this(0);
+	}
+	public UnsignedByte(int i)
+	{
+		if(checkData(i))
+		{
+			setData(i);
+		}
+	}
+	public short getData()
+	{
+		return (short) (Data & 0xFFFF);
+	}
+	public void setData(int i)
+	{
+		Data = (short) (0xFFFF | i);
+	}
+	public boolean checkData(int i)
+	{
+		if(i >= Math.pow(2, 8))
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+
+}
