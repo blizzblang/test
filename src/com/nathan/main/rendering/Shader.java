48f08db4f48b4c11cc3d9f1c9bbe27e11de0e91e
blizzblang <blizzblang@users.noreply.github.com>
2016-01-19 11:54:20 -0600

    Adding Shader Class
    This will be useful later when working on rendering with VBO's and VAO's

---
+++ src/com/nathan/main/rendering/Shader.java
@@ -0,0 +1,17 @@
+public abstract class Shader()
+{
+  private String Frag="";
+  private String Vert="";
+  public Shader(String frag,String vert)
+  {
+  Frag=frag;
+  Vert=vert;
+  init();
+  }
+  private void init()
+  {
+  
+  }
+  public abstract void Bind();
+  public abstract void unBind();
+}
