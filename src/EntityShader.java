1f957451d0811e89c75ae4c72b0fb313e5f560ea
Nathan <Nathan@192.168.2.38>
2016-01-19 18:48:16 -0600

    More Shader nonsense

---
+++ src/EntityShader.java
@@ -0,0 +1,23 @@
+import com.nathan.main.rendering.Shader;
+
+public class EntityShader extends Shader
+{
+
+	public EntityShader() {
+		super("","");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void Bind() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void unBind() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
