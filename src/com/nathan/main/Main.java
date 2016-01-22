80ace2c95ea840ea165862883195ca35258edb42
blizzblang <blizzblang@users.noreply.github.com>
2016-01-22 10:53:06 -0600

    Added Model

--- src/com/nathan/main/Main.java
+++ src/com/nathan/main/Main.java
@@ -15,16 +15,9 @@ public class Main {
 	{
 			OpenGL.initOpenGL(800, 600);
 			
-			VBO test = new VBO();
-			test.addTexture(ImgLoader.loadImage("GameFiles/Image/sinstar/sinstar.png"));
-			Vertex v0 = new Vertex(); 
-	        v0.setXYZ(-0.5f, 0.5f, 0); v0.setRGB(1, 0, 0); v0.setST(0, 0);
-	        Vertex v1 = new Vertex(); 
-	        v1.setXYZ(-0.5f, -0.5f, 0); v1.setRGB(0, 1, 0); v1.setST(0, 1);
-	        Vertex v2 = new Vertex(); 
-	        v2.setXYZ(0.5f, -0.5f, 0); v2.setRGB(0, 0, 1); v2.setST(1, 1);
-	        Vertex v3 = new Vertex(); 
-	        v3.setXYZ(0.5f, 0.5f, 0); v3.setRGB(1, 1, 1); v3.setST(1, 0);
+		VBO test = new VBO();
+		test.addTexture(ImgLoader.loadImage("GameFiles/Image/sinstar/sinstar.png"));
+		Model test = new Model("GameFiles/Model/box/box.obj",test);
 	        test.addVertex(v0);
 	        test.addVertex(v1);
 	        test.addVertex(v2);
